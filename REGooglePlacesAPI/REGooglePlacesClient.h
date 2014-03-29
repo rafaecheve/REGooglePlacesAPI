10f453a4989e4bf7316760f9ef91eb3b6dd788f5
rafael echeverria <rafael.echverria@gmail.com>
2014-03-28 20:32:03 -0600

    adding license comments

--- REGooglePlacesAPI/REGooglePlacesClient.h
+++ REGooglePlacesAPI/REGooglePlacesClient.h
@@ -19,11 +19,13 @@ static NSString * const REGooglePlaceAPIBaseURL = @"https://maps.googleapis.com/
 
 @interface REGooglePlacesClient : AFHTTPSessionManager
 
+
 + (REGooglePlacesClient *)sharedGooglePlacesClient;
 
 @property (nonatomic, weak) id<REGooglePlacesClientDelegate>delegate;
 
 - (void)REGooglePlaceSearchRequest:(REGooglePlaceSearch *)search;
+
 - (void)REGooglePlaceDetail:(REGooglePlace *)place withSearchRequest:(REGooglePlaceSearch *)search;
 //- (void)REGooglePlaceNearBySearchByTerm:(NSString *)searchTerm;
 //- (void)REGooglePlaceTextSearchByTerm:(NSString *)searchTerm;
@@ -33,17 +35,23 @@ static NSString * const REGooglePlaceAPIBaseURL = @"https://maps.googleapis.com/
 
 @protocol REGooglePlacesClientDelegate <NSObject>
 
+
 @optional
 
+/* Returns an array of places based on the client request
+*/
 -(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFoundPlaces:(NSArray *)places;
+
+/* Returns a place detail based on the client request
+ */
 -(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFoundPlace:(REGooglePlace *)place;
 
+/* Returns a fail message based on the client request
+ */
 -(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFailWithMessage:(NSString *)message;
 
-//-(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFoundNearByPlaces:(NSArray *)places;
-//-(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFoundTextSearchPlaces:(NSArray *)places;
-//-(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFoundRadarSearchPlaces:(NSArray *)places;
-
+/* Returns a fail NSError object based on the client request
+ */
 -(void)REGooglePlacesClient:(REGooglePlacesClient *)client didFailWithError:(NSError *)error;
 
 @end
